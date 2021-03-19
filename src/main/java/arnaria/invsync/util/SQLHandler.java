6bc69856f634a9621b42043eb468fc1e62a180af
MrNavaStar <43613843+MrNavaStar@users.noreply.github.com>
2021-03-19 09:52:16 -0600

    Update SQLHandler.java

--- src/main/java/arnaria/invsync/util/SQLHandler.java
+++ src/main/java/arnaria/invsync/util/SQLHandler.java
@@ -6,11 +6,9 @@ import java.sql.SQLException;
 
 public class SQLHandler {
 
-    public static void connectToSQL() {
-        String url = "jdbc:mysql://192.168.1.87:3306/invsync";
-        String username = "root";
-        String password = "1234";
+    private static String url, username, password, table;
 
+    public static void connectToSQL() {
         System.out.println("Connecting database...");
 
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
@@ -18,5 +16,37 @@ public class SQLHandler {
         } catch (SQLException e) {
             System.out.println("Cannot connect the database!");
         }
+    }
+
+    public static void getConfigData() {
+        String databaseName = ConfigManager.SQL_Database_Name;
+        table = ConfigManager.SQL_Database_Table_Name;
+        String address = ConfigManager.SQL_Server_Address;
+        String port = ConfigManager.SQL_Server_Port;
+        username = ConfigManager.SQL_User;
+        password = ConfigManager.SQL_Password;
+
+        url = "jdbc:mysql://" + address + ":" + port + "/" + databaseName;
+    }
+
+    public static void start() {
+        getConfigData();
+        connectToSQL();
+    }
+
+    public static void createTable() {
+        
+    }
+
+    public static void createRow() {
+
+    }
+
+    public static void readTableRow(String row) {
+
+    }
+
+    public static void writeTableRow(String row) {
+
     }
 }
