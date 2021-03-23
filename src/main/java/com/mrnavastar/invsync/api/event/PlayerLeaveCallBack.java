5c3818c1cfeaab99f3400121a7af2b42824c8644
ethans0061 <Mr.NavaStar@gmail.com>
2021-03-22 21:09:46 -0600

    fixed stuff

--- src/main/java/com/mrnavastar/invsync/api/event/PlayerLeaveCallBack.java
+++ src/main/java/com/mrnavastar/invsync/api/event/PlayerLeaveCallBack.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/main/java/com/mrnavastar/invsync/api/event/PlayerLeaveCallBack.java
 package com.mrnavastar.invsync.api.event;
 
 import net.fabricmc.fabric.api.event.Event;
@@ -22,28 +21,4 @@ public interface PlayerLeaveCallBack {
 
     ActionResult leaveServer(ServerPlayerEntity player, MinecraftServer server);
 }
-=======
-package com.mrnavastar.invsync.api.event;
 
-import net.fabricmc.fabric.api.event.Event;
-import net.fabricmc.fabric.api.event.EventFactory;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.util.ActionResult;
-
-public interface PlayerLeaveCallBack {
-    Event<PlayerLeaveCallBack> EVENT = EventFactory.createArrayBacked(PlayerLeaveCallBack.class, (listeners) -> (player, server) -> {
-        for (PlayerLeaveCallBack listener : listeners) {
-            ActionResult result = listener.leaveServer(player, server);
-
-            if (result != ActionResult.PASS) {
-                return result;
-            }
-        }
-
-        return ActionResult.PASS;
-    });
-
-    ActionResult leaveServer(ServerPlayerEntity player, MinecraftServer server);
-}
->>>>>>> 07023deeb41fdb819ebcd94f4511a21104422eee:src/main/java/arnaria/invsync/api/event/PlayerLeaveCallBack.java
--- src/main/java/com/mrnavastar/invsync/util/NBTtoSQL.java
+++ src/main/java/com/mrnavastar/invsync/util/NBTtoSQL.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/main/java/com/mrnavastar/invsync/util/NBTtoSQL.java
 package com.mrnavastar.invsync.util;
 
 import net.minecraft.entity.player.PlayerEntity;
@@ -56,62 +55,3 @@ public class NBTtoSQL {
     }
 }
 
-=======
-package com.mrnavastar.invsync.util;
-
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-
-import java.util.UUID;
-
-public class NBTtoSQL {
-
-    public static void convertInventory(PlayerEntity player, UUID uuid) {
-        if (ConfigManager.Sync_Inv) {
-            for (int i = 0; i < 36; i++) {
-                ItemStack item = player.inventory.main.get(i);
-                System.out.println("inv " + i + ": " + item);
-            }
-            ItemStack item = player.inventory.offHand.get(0);
-            System.out.println("Off Hand: " + item);
-        }
-
-        if (ConfigManager.Sync_Armour) {
-            for (int i = 0; i < 4; i++) {
-                ItemStack item = player.inventory.armor.get(i);
-                System.out.println("armour: " + item);
-            }
-        }
-    }
-
-    public static void convertStats(PlayerEntity player, UUID uuid) {
-        int xp, score, foodLevel;
-        float health, saturation;
-
-        if (ConfigManager.Sync_Xp) xp = player.experienceLevel;
-        if (ConfigManager.Sync_Score) score = player.getScore();
-        if (ConfigManager.Sync_Health) health = player.getHealth();
-        if (ConfigManager.Sync_Food_Level)
-            foodLevel = player.getHungerManager().getFoodLevel();
-            saturation = player.getHungerManager().getSaturationLevel();
-    }
-
-    public static void convertEnderChest(PlayerEntity player, UUID uuid) {
-        if (ConfigManager.Sync_eChest) {
-            for (int i = 0; i < 28; i++) {
-                ItemStack item = player.getEnderChestInventory().getStack(i);
-                System.out.println("ender: " + i + ": " + item);
-            }
-        }
-    }
-
-    public static void convert(PlayerEntity player) {
-        UUID uuid = player.getUuid();
-
-        convertInventory(player, uuid);
-        convertEnderChest(player, uuid);
-        convertStats(player, uuid);
-    }
-}
-
->>>>>>> 07023deeb41fdb819ebcd94f4511a21104422eee:src/main/java/arnaria/invsync/util/NBTtoSQL.java
