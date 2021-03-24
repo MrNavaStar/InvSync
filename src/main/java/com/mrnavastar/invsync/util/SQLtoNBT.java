0f4c1bd4c229460fd8ebb62c6edb967684f6dda2
ethans0061 <Mr.NavaStar@gmail.com>
2021-03-23 23:37:03 -0600

    Update SQLtoNBT.java

--- src/main/java/com/mrnavastar/invsync/util/SQLtoNBT.java
+++ src/main/java/com/mrnavastar/invsync/util/SQLtoNBT.java
@@ -1,5 +1,9 @@
 package com.mrnavastar.invsync.util;
 
+import net.minecraft.item.ItemStack;
+
 public class SQLtoNBT {
+    String itemRaw = "1 dirt";
+    String[] splitArray = itemRaw.split(" ");
 }
 
