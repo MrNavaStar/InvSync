01ef125fd8407b3b2c2290920e63012573c24d0b
ethans0061 <Mr.NavaStar@gmail.com>
2021-03-23 23:16:16 -0600

    NBT to SQL works!

--- src/main/java/com/mrnavastar/invsync/util/NBTtoSQL.java
+++ src/main/java/com/mrnavastar/invsync/util/NBTtoSQL.java
@@ -3,51 +3,55 @@ package com.mrnavastar.invsync.util;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 
-import java.util.UUID;
-
 public class NBTtoSQL {
 
-    public static void convertInventory(PlayerEntity player, UUID uuid) {
-        if (ConfigManager.Sync_Inv) {
-            for (int i = 0; i < 36; i++) {
-                ItemStack item = player.inventory.main.get(i);
-                System.out.println("inv " + i + ": " + item);
-            }
-            ItemStack item = player.inventory.offHand.get(0);
-            System.out.println("Off Hand: " + item);
+    public static void convertInventory(PlayerEntity player, String uuid) {
+        for (int i = 0; i < 36; i++) {
+            ItemStack item = player.inventory.main.get(i);
+            SQLHandler.saveItem(uuid, "inv", i, item);
         }
+        ItemStack item = player.inventory.offHand.get(0);
+        SQLHandler.saveItem(uuid, "offHand", item);
 
-        if (ConfigManager.Sync_Armour) {
-            for (int i = 0; i < 4; i++) {
-                ItemStack item = player.inventory.armor.get(i);
-                System.out.println("armour: " + item);
+        for (int i = 0; i < 4; i++) {
+            item = player.inventory.armor.get(i);
+            SQLHandler.saveItem(uuid, "armour", i, item);
+        }
+    }
+
+    public static void convertEnderChest(PlayerEntity player, String uuid) {
+        if (ConfigManager.Sync_eChest) {
+            for (int i = 0; i < 28; i++) {
+                ItemStack item = player.getEnderChestInventory().getStack(i);
+                SQLHandler.saveItem(uuid, "eChest", i, item);
             }
         }
     }
 
-    public static void convertStats(PlayerEntity player, UUID uuid) {
+    public static void convertStats(PlayerEntity player, String uuid) {
         int xp, score, foodLevel;
         float health, saturation;
 
-        if (ConfigManager.Sync_Xp) xp = player.experienceLevel;
-        if (ConfigManager.Sync_Score) score = player.getScore();
-        if (ConfigManager.Sync_Health) health = player.getHealth();
+        if (ConfigManager.Sync_Xp) {
+            xp = player.experienceLevel;
+            SQLHandler.saveInt(uuid, "xp", xp);
+        }
+        if (ConfigManager.Sync_Score) {
+            score = player.getScore();
+            SQLHandler.saveInt(uuid, "score", score);
+        }
+        if (ConfigManager.Sync_Health) {
+            health = player.getHealth();
+            SQLHandler.saveFloat(uuid, "health", health);
+        }
         if (ConfigManager.Sync_Food_Level)
             foodLevel = player.getHungerManager().getFoodLevel();
             saturation = player.getHungerManager().getSaturationLevel();
     }
 
-    public static void convertEnderChest(PlayerEntity player, UUID uuid) {
-        if (ConfigManager.Sync_eChest) {
-            for (int i = 0; i < 28; i++) {
-                ItemStack item = player.getEnderChestInventory().getStack(i);
-                System.out.println("ender: " + i + ": " + item);
-            }
-        }
-    }
-
     public static void convert(PlayerEntity player) {
-        UUID uuid = player.getUuid();
+        String uuid = player.getUuid().toString();
+        SQLHandler.createRow(uuid);
 
         convertInventory(player, uuid);
         convertEnderChest(player, uuid);
--- src/main/java/com/mrnavastar/invsync/util/SQLHandler.java
+++ src/main/java/com/mrnavastar/invsync/util/SQLHandler.java
@@ -1,29 +1,30 @@
 package com.mrnavastar.invsync.util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 import net.minecraft.item.ItemStack;
 import org.apache.logging.log4j.Level;
+
 import static com.mrnavastar.invsync.Invsync.log;
 
 public class SQLHandler {
 
-    public static String databaseName, tableName, databaseDirectory, url;
+    private static String databaseName, tableName, databaseDirectory;
+    private static StringBuilder columns;
     public static Connection connection;
 
-    public static void getConfigData() {
+    //Get config data from ConfigHandler
+    private static void getConfigData() {
         databaseName = "database.db";
         tableName = "PlayerData";
         databaseDirectory = "C:/sqlite";
     }
 
+    //Open connection to database
     public static void connect() {
         try {
             Class.forName("org.sqlite.JDBC");
-            url = "jdbc:sqlite:C:" + databaseDirectory + "/" + databaseName;
+            String url = "jdbc:sqlite:" + databaseDirectory + "/" + databaseName;
 
             connection = DriverManager.getConnection(url);
             log(Level.INFO,"Successfully connected to database!");
@@ -34,6 +35,7 @@ public class SQLHandler {
         }
     }
 
+    //Close connection to database
     public static void disconnect() {
         try {
             connection.close();
@@ -42,12 +44,8 @@ public class SQLHandler {
         }
     }
 
-    public static void createTable() {
-        String sql = "CREATE TABLE IF NOT EXISTS " + tableName + " (\n"
-                + "	id integer PRIMARY KEY,\n"
-                + "	name text NOT NULL,\n"
-                + "	capacity real\n"
-                + ");";
+    //Run sql statement
+    private static void executeStatement(String sql) {
         try {
             Statement stmt = connection.createStatement();
             stmt.execute(sql);
@@ -57,9 +55,70 @@ public class SQLHandler {
         }
     }
 
+    //Create columns for table
+    private static void createColumns() {
+        columns = new StringBuilder();
+
+        for (int i = 0; i < 36; i++) {
+            columns.append("inv").append(i).append(" TEXT,");
+        }
+        columns.append("offHand").append(" TEXT,");
+
+        for (int i = 0; i < 4; i++) {
+            columns.append("armour").append(i).append(" TEXT,");
+        }
+
+        for (int i = 0; i < 28; i++) {
+            columns.append("eChest").append(i).append(" TEXT,");
+        }
+
+        columns.append("xp").append(" INTEGER,");
+        columns.append("score").append(" INTEGER,");
+        columns.append("health").append(" REAL,");
+        columns.append("foodLevel").append(" INTEGER,");
+        columns.append("saturation").append(" REAL");
+    }
+
+    //Create table if one does not exist
+    private static void createTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS " + tableName + " (uuid TEXT PRIMARY KEY, " + columns + ");";
+        executeStatement(sql);
+    }
+
+    //Create row
+    public static void createRow(String uuid) {
+        String sql = "INSERT OR REPLACE INTO " + tableName + "(uuid) VALUES('" + uuid + "');";
+        executeStatement(sql);
+    }
+
+    //Save item to row at name + index
+    public static void saveItem(String uuid, String name, int index, ItemStack itemStack) {
+        String sql = "UPDATE " + tableName + " SET " + name + index + " = '" + itemStack + "' WHERE uuid = '" + uuid + "'";
+        executeStatement(sql);
+    }
+
+    //Save item to row at name
+    public static void saveItem(String uuid, String name, ItemStack itemStack) {
+        String sql = "UPDATE " + tableName + " SET " + name + " = '" + itemStack + "' WHERE uuid = '" + uuid + "'";
+        executeStatement(sql);
+    }
+
+    //Save int to row at name
+    public static void saveInt(String uuid, String name, int amount) {
+        String sql = "UPDATE " + tableName + " SET " + name + " = " + amount + " WHERE uuid = '" + uuid + "'";
+        executeStatement(sql);
+    }
+
+    //Save float to row at name
+    public static void saveFloat(String uuid, String name, float amount) {
+        String sql = "UPDATE " + tableName + " SET " + name + " = " + amount + " WHERE uuid = '" + uuid + "'";
+        executeStatement(sql);
+    }
+
     public static void start() {
         getConfigData();
         connect();
+        createColumns();
         createTable();
     }
 }
