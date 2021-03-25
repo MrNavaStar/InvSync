f533c15f7065f6d13f771d594fafda42b3f7e03e
MrNavaStar <43613843+MrNavaStar@users.noreply.github.com>
2021-03-25 09:24:57 -0600

    Idk I think it works

--- src/main/java/com/mrnavastar/invsync/util/ConversionHelpers.java
+++ src/main/java/com/mrnavastar/invsync/util/ConversionHelpers.java
@@ -7,13 +7,11 @@ public class ConversionHelpers {
 
     public static String itemStackToString(ItemStack item) {
         String itemData = item.toTag(new CompoundTag()).toString();
-        itemData = itemData.replace("'", "$");
-        return itemData;
+        return itemData.replace("'", "$");
     }
-    
+
     public static ItemStack stringToItemStack(String sqlData) {
         sqlData = sqlData.replace("$", " ");
-        ItemStack itemStack = ItemStack.fromTag(new CompoundTag().getCompound(sqlData));
-        return itemStack;
+        return ItemStack.fromTag(new CompoundTag().getCompound(sqlData));
     }
 }
