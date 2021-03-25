e4a6276808ae379cf1d7241cbcd003c80c3e9274
MrNavaStar <43613843+MrNavaStar@users.noreply.github.com>
2021-03-25 09:23:07 -0600

    Converts between item stack and string correctly (Maybe)

--- src/main/java/com/mrnavastar/invsync/util/ConversionHelpers.java
+++ src/main/java/com/mrnavastar/invsync/util/ConversionHelpers.java
@@ -1,13 +1,19 @@
 package com.mrnavastar.invsync.util;
 
 import net.minecraft.item.ItemStack;
-import org.apache.commons.lang3.StringUtils;
+import net.minecraft.nbt.CompoundTag;
 
 public class ConversionHelpers {
 
     public static String itemStackToString(ItemStack item) {
-        String itemData = item.getItem() + " " + item.getCount() + " " + item.getTag();
+        String itemData = item.toTag(new CompoundTag()).toString();
         itemData = itemData.replace("'", "$");
         return itemData;
+    }
+    
+    public static ItemStack stringToItemStack(String sqlData) {
+        sqlData = sqlData.replace("$", " ");
+        ItemStack itemStack = ItemStack.fromTag(new CompoundTag().getCompound(sqlData));
+        return itemStack;
     }
 }
