ab10b09afd74892077aba9b8b9dce5f731677c42
MrNavaStar <Mr.NavaStar@gmail.com>
2021-06-15 00:47:33 -0600

    Updated to 1.17

--- src/main/java/com/mrnavastar/invsync/sql/Table.java
+++ src/main/java/com/mrnavastar/invsync/sql/Table.java
@@ -41,9 +41,12 @@ public class Table {
 
     public void startTransaction() {
         SQLHandler.connect();
+        SQLHandler.executeStatement("PRAGMA cache_size=10000;");
+        SQLHandler.executeStatement("BEGIN TRANSACTION;");
     }
 
     public void endTransaction() {
+        SQLHandler.executeStatement("COMMIT;");
         SQLHandler.disconnect();
     }
 
