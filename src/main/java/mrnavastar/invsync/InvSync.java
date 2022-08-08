57f804f36304eacf00d7ee132ac5dbfa547f3bd6
MrNavaStar <Mr.NavaStar@gmail.com>
2022-08-07 21:38:00 -0600

    Refactor pr

--- src/main/java/mrnavastar/invsync/InvSync.java
+++ src/main/java/mrnavastar/invsync/InvSync.java
@@ -1,7 +1,6 @@
 package mrnavastar.invsync;
 
 import mc.microconfig.MicroConfig;
-import mrnavastar.invsync.api.ServerSaveEvent;
 import mrnavastar.invsync.api.ServerSyncEvents;
 import mrnavastar.invsync.services.CoreSyncProcedures;
 import mrnavastar.invsync.services.Settings;
@@ -26,6 +25,7 @@ public class InvSync implements ModInitializer {
     public static final String MODID = "InvSync";
     public static Settings settings;
     private static Database database;
+    public static Table playerData;
     public static ServerAdvancementLoader advancementLoader;
 
     @Override
@@ -42,21 +42,16 @@ public class InvSync implements ModInitializer {
             }
 
             database = new SQLiteDatabase(settings.DATABASE_NAME, settings.SQLITE_DIRECTORY);
-            validConfig = true;
-        }
 
-        if (settings.DATABASE_TYPE.equals("MYSQL") && !settings.MYSQL_USERNAME.equals("username") && !settings.MYSQL_PASSWORD.equals("password")) {
+        } else if (settings.DATABASE_TYPE.equals("MYSQL") && !settings.MYSQL_USERNAME.equals("username") && !settings.MYSQL_PASSWORD.equals("password")) {
             database = new MySQLDatabase(settings.DATABASE_NAME, settings.MYSQL_ADDRESS, settings.MYSQL_PORT, settings.MYSQL_USERNAME, settings.MYSQL_PASSWORD);
-            validConfig = true;
-        }
-
-        if (!validConfig) {
+        } else {
             log(Level.FATAL, "Halting initialization! You need to change some settings in the InvSync config");
             System.exit(0);
         }
 
-        Table playerData = database.createTable(MODID + "-PlayerData");
-        Table serverData = database.createTable(MODID + "-ServerData");
+        playerData = database.createTable("PlayerData");
+        Table serverData = database.createTable("ServerData");
         log(Level.INFO, "Database initialized successfully!");
 
         ServerLifecycleEvents.SERVER_STARTING.register(server -> advancementLoader = server.getAdvancementLoader());
@@ -94,19 +89,6 @@ public class InvSync implements ModInitializer {
             ServerSyncEvents.SAVE_SERVER_DATA.invoker().handle(server, serverData);
             serverData.endTransaction();
         }));
-
-        ServerSaveEvent.SAVE_EVERYTHING_EVENT.register(server -> {
-            playerData.beginTransaction();
-            for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
-                DataContainer playerDataContainer = playerData.get(player.getUuid());
-                if (playerDataContainer == null) {
-                    playerDataContainer = playerData.createDataContainer(player.getUuid());
-                    playerData.put(playerDataContainer);
-                }
-                ServerSyncEvents.SAVE_PLAYER_DATA.invoker().handle(player, playerDataContainer);
-            }
-            playerData.endTransaction();
-        });
 
         CoreSyncProcedures.init();
         log(Level.INFO, "Complete!");
--- src/main/java/mrnavastar/invsync/mixin/MinecraftServerMixin.java
+++ src/main/java/mrnavastar/invsync/mixin/MinecraftServerMixin.java
@@ -1,19 +1,33 @@
 package mrnavastar.invsync.mixin;
 
-import mrnavastar.invsync.api.ServerSaveEvent;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import mrnavastar.invsync.api.ServerSyncEvents;
+import mrnavastar.sqlib.api.DataContainer;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.PlayerManager;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import static mrnavastar.invsync.InvSync.playerData;
+
 @Mixin(MinecraftServer.class)
-public class MinecraftServerMixin {
+public abstract class MinecraftServerMixin {
+
+    @Shadow public abstract PlayerManager getPlayerManager();
 
     @Inject(method = "saveAll", at = @At("HEAD"))
     public void onSave(boolean suppressLogs, boolean flush, boolean force, CallbackInfoReturnable<Boolean> cir) {
-        ServerSaveEvent.SAVE_EVERYTHING_EVENT.invoker().onSaveEverything((MinecraftServer) (Object) this);
+        playerData.beginTransaction();
+        this.getPlayerManager().getPlayerList().forEach(player -> {
+            DataContainer playerDataContainer = playerData.get(player.getUuid());
+            if (playerDataContainer == null) {
+                playerDataContainer = playerData.createDataContainer(player.getUuid());
+                playerData.put(playerDataContainer);
+            }
+            ServerSyncEvents.SAVE_PLAYER_DATA.invoker().handle(player, playerDataContainer);
+        });
+        playerData.endTransaction();
     }
-
 }
